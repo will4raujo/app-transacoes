bc9ae162d44ecf5358a528810d5e73ba89277756
willian <willian.araujo1994@gmail.com>
2024-06-15 13:04:20 -0300

    feat: html-auth
    * creating default layout for auth pages
    * creating filling fields for the signin and signup forms
    * new inputText component
    * new client SubmitButton component

--- frontend-teste-fastpay/src/app/(auth)/sign-in/page.tsx
+++ frontend-teste-fastpay/src/app/(auth)/sign-in/page.tsx
@@ -1,8 +1,22 @@
-'use client';
+import SubmitButton from '../../../components/submitButton'
+import InputText from '../../../components/inputText'
+
+import Link from 'next/link'
 
 export default function SignIn() {
 
   return (
-    <h1>SignIn</h1>
+    <>
+      <legend className="text-white">Faça seu login</legend>
+
+      <InputText placeholder="E-mail"/>
+      <InputText placeholder="Senha"/>
+
+      <SubmitButton text="Entrar"/>
+
+      <Link href="/sign-up" className='text-white'>
+        Ainda não tenho uma conta
+      </Link>
+    </>
   )
 }
--- frontend-teste-fastpay/src/app/(auth)/sign-up/page.tsx
+++ frontend-teste-fastpay/src/app/(auth)/sign-up/page.tsx
@@ -1,6 +1,23 @@
+import SubmitButton from '../../../components/submitButton'
+import InputText from '../../../components/inputText'
+
+import Link from 'next/link'
+
 export default function SignUp() {
 
   return (
-    <h1>SignUp</h1>
+    <>
+      <legend className="text-white">Crie sua conta</legend>
+      
+      <InputText placeholder="Nome"/>
+      <InputText placeholder="E-mail"/>
+      <InputText placeholder="Senha"/>
+
+      <SubmitButton text="Cadastrar"/>
+
+      <Link href="/sign-in" className='text-white'>
+        Já tenho uma conta
+      </Link>
+    </>
   )
 }
