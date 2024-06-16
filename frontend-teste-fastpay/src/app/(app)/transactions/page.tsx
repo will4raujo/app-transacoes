7b636ee5dd65faa25489787d31a5e0e720b905b6
willian <willian.araujo1994@gmail.com>
2024-06-16 20:04:23 -0300

    feat: mobile responsive
    * removed eye icon
    * adjusted max width sreen from table
    * hidde category cell for mobile screen

--- frontend-teste-fastpay/src/app/(app)/transactions/page.tsx
+++ frontend-teste-fastpay/src/app/(app)/transactions/page.tsx
@@ -1,13 +1,13 @@
 import { Suspense } from 'react'
 import Link from 'next/link'
-import {PenLine, Trash, Eye} from 'lucide-react'
+import {PenLine, Trash} from 'lucide-react'
 
 export default function Transactions() {
   return (
-    <main className='w-full px-4 md:px-10 py-5 flex flex-col gap-5 mb-[80px] md:mx-auto max-w-[800px]'>
+    <main className='w-full px-4 md:px-10 py-5 flex flex-col gap-5 mb-[80px] md:mx-auto max-w-[1024px]'>
+      <div className='flex flex-col md:flex-row gap-4 justify-between'>
       <h1 className='text-3xl font-bold'>Transações financeiras</h1>
-      <div className='flex gap-4 justify-end'>
-        <Link href='/transactions/new' passHref className='bg-zinc-500 min-w-[120px] h-[48px] p-2 text-white rounded-md hover:bg-zinc-600 transition-colors duration-200'>Nova transação</Link>
+        <Link href='/transactions/new' passHref className='bg-zinc-500 min-w-[120px] h-[45px] px-2 pt-2 pb-0 text-white rounded-md text-center hover:bg-zinc-600 transition-colors duration-200'>Nova transação</Link>
       </div>
       <Suspense fallback={<p>Carregando lista de transações...</p>}>
         <table className='w-full'>
@@ -16,7 +16,7 @@ export default function Transactions() {
               <th className='p-2'>Descrição</th>
               <th className='p-2'>Valor</th>
               <th className='p-2'>Data</th>
-              <th className='p-2'>Categoria</th>
+              <th className='p-2 hidden md:table-cell'>Categoria</th>
               <th className='p-2'>Ações</th>
             </tr>
           </thead>
@@ -26,11 +26,8 @@ export default function Transactions() {
                 <td className='p-2 text-center'>{transaction.description}</td>
                 <td className='p-2 text-center'>{transaction.value}</td>
                 <td className='p-2 text-center'>{transaction.date}</td>
-                <td className='p-2 text-center'>{transaction.category}</td>
+                <td className='p-2 text-center hidden md:table-cell'>{transaction.category}</td>
                 <td className='p-2 flex gap-2 justify-center'>
-                  <button className='bg-zinc-900 p-1 rounded-md hover:bg-zinc-600 transition-colors   duration-200'>
-                    <Eye size={24} />
-                  </button>
                   <button className='bg-zinc-900 p-1 rounded-md hover:bg-zinc-600 transition-colors duration-200'>
                     <PenLine size={24} />
                   </button>
