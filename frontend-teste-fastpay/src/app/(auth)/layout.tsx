7382e91af2c6a9359aea2ef2167625bf9d3578df
willian <willian.araujo1994@gmail.com>
2024-06-18 19:29:27 -0300

    feat: LogIn authentication
    - imported zustand for states and requests
    - Adjusted form container in pages signIn and SignOut
    - created auth hook for authentication

--- frontend-teste-fastpay/src/app/(auth)/layout.tsx
+++ frontend-teste-fastpay/src/app/(auth)/layout.tsx
@@ -1,11 +1,9 @@
 import { ReactNode } from 'react'
 
-export default function AuthLayout({ children } : { children: ReactNode }) {
+export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
     <div className='grid min-h-screen place-items-center'>
-      <form className='flex flex-col w-[350px] md:w-[478px] gap-6 bg-black/60 border-zinc-900 border-2 p-5 md:p-10 rounded-md'>
-        {children}
-      </form>
+      {children}
     </div>
   )
 }
