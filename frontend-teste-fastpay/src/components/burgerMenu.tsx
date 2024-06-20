f36d7ad740466b33b849e3caa196d82b3b625736
willian <willian.araujo1994@gmail.com>
2024-06-20 10:34:41 -0300

    fix: close menu when clicked

--- frontend-teste-fastpay/src/components/burgerMenu.tsx
+++ frontend-teste-fastpay/src/components/burgerMenu.tsx
@@ -18,8 +18,8 @@ export default function BurguerMenu({ children }: { children: ReactNode }) {
         <span className='text-white text-lg'>Menu</span>
       </div>
     { isOpen && (
-        <div className='fixed left-0 top-0 h-screen w-60 bg-black/90 flex flex-col justify-start gap-4 py-2 px-4'>
-          <div className='close-icon relative cursor-pointer flex flex-col justify-between w-8 h-8 mt-1' onClick={handleMenu}> 
+        <div className='fixed left-0 top-0 h-screen w-60 bg-black/90 flex flex-col justify-start gap-4 py-2 px-4' onClick={handleMenu}>
+          <div className='close-icon relative cursor-pointer flex flex-col justify-between w-8 h-8 mt-1'> 
             <X size={20} />
           </div>
           {children}
