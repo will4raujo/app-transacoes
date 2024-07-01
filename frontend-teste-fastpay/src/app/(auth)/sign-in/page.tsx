9fec991d8da8c51811791ff898fa0050f0796bea
willian <willian.araujo1994@gmail.com>
2024-07-01 10:51:08 -0300

    fix: auth pages as server component
    - created new client components signUpForm and signInForm
    - auth pages now is rendering in server side
    - added metadata in auth pages

--- frontend-teste-fastpay/src/app/(auth)/sign-in/page.tsx
+++ frontend-teste-fastpay/src/app/(auth)/sign-in/page.tsx
@@ -1,43 +1,12 @@
-'use client'
-import SubmitButton from '../../../components/submitButton'
-import InputText from '../../../components/inputText'
-import Link from 'next/link'
-import { useState } from 'react'
-import { useAuthHook } from '@/hooks/auth.hook'
-import { useRouter } from 'next/navigation'
-import { useAsyncHook } from '@/hooks/async.hook'
+import SignInForm from "@/components/signInForm"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: 'login',
+}
 
 export default function SignIn() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const routes = useRouter()
-
-  const { isLoading } = useAsyncHook()
-
-  const { login } = useAuthHook((state ) => ({
-    login: state.login,
-  }))
-  
-  async function handleSignIn(event: React.FormEvent<HTMLFormElement>) {
-    event?.preventDefault()
-    const isAthenticated = await login({ email, password })
-    if (isAthenticated) {
-      routes.push('/')
-    } 
-  }
-
   return (
-    <form onSubmit={handleSignIn} className='flex flex-col w-[350px] md:w-[478px] gap-6 bg-black/60 border-zinc-900 border-2 p-5 md:p-10 rounded-md'>
-      <legend className="text-white self-center">Faça seu login</legend>
-
-      <InputText placeholder="E-mail" required onChange={(e) => setEmail(e.target.value)}/>
-      <InputText placeholder="Senha" type="password" required onChange={(e) => setPassword(e.target.value)}/>
-
-      <SubmitButton text="Entrar" type='submit' loading={isLoading}/>
-
-      <Link href="/sign-up" className='text-white self-center hover:underline'>
-        Ainda não tenho uma conta
-      </Link>
-    </form>
+    <SignInForm />
   )
 }
